efcfa728b8b36bfbaad1738d2b2b3a45d91a7163
Andrew <andrew_shliahta@ukr.net>
2021-09-05 20:44:30 +0300

    Aded expend item

--- src/index.js
+++ src/index.js
@@ -40,17 +40,18 @@ addToDataFromStorage();
 
 function createMarkup (books) {
     books.reduce((acc, item) => {
-        acc += `<li class="bookItem" id="${item['id']}">
-                    <img src="#" alt="Обкладинка книги" class="bookPhoto_bookItem bookItem-elements hidden">          
-                    <div class="book-content-wrap">
-                        <p class="bookName_bookItem bookItem-elements hidden">Book name: ${item['bookName']}</p>
-                        <p class="authorName_bookItem bookItem-elements">Author name: ${item['authorName']}</p>
-                        <p class="litWork_bookItem bookItem-elements">Literary work: ${item['litWork']}</p>
-                        <p class="publicYear_bookItem bookItem-elements hidden">Publication year: ${item['publicYear']}</p>
+        acc += `<li class="bookItem collapse-expand" id="${item['id']}">
+                    <img src="#" alt="Обкладинка книги" class="bookPhoto_bookItem bookItem-elements show_hide hidden">          
+                    <div class="book-content-wrap  collapse-expand">
+                        <p class="bookName_bookItem bookItem-elements show_hide hidden"><span>Book name:</span> ${item['bookName']}</p>
+                        <p class="authorName_bookItem bookItem-elements"><span class="text show_hide hidden">Author name:</span> ${item['authorName']}</p>
+                        <p class="litWork_bookItem bookItem-elements"><span class="text show_hide hidden">Literary work:</span> ${item['litWork']}</p>
+                        <p class="publicYear_bookItem bookItem-elements show_hide hidden"><span>Publication year:</span> ${item['publicYear']}</p>
                     </div>
                     <div class="button-item-wrap">
-                        <div class="removeBook hidden">Remove a book</div>
-                        <div class="changeDataBook hidden">Change data book</div>
+                        <div class="bookItem-elements close show_hide hidden">X</div>
+                        <div class="bookItem-elements removeBook show_hide hidden">Remove a book</div>
+                        <div class="bookItem-elements changeDataBook show_hide hidden">Change data book</div>
                     </div>
                 </li>`;
         return refs.booksList.innerHTML = acc;
@@ -74,16 +75,25 @@ const addBook = function (e) {
 
 
 function expandItem(e) {
+    if(e.target.nodeName!=="P"){return}
     const chosenLiElem = e.target.closest(".bookItem");
-    // const chosenElem = data.books.find(elem => { return elem.id === chosenLiElem.id });
 
-    chosenLiElem.classList.add('expand-item');
-    chosenLiElem.querySelector('.bookPhoto_bookItem').classList.remove('hidden');
-    chosenLiElem.querySelector('.bookName_bookItem').classList.remove('hidden');
-    chosenLiElem.querySelector('.publicYear_bookItem').classList.remove('hidden');
-    chosenLiElem.querySelector('.removeBook').classList.remove('hidden');
-    chosenLiElem.querySelector('.changeDataBook').classList.remove('hidden');
+    chosenLiElem.querySelectorAll('.show_hide').forEach(element => {
+        element.classList.remove('hidden');
+    });
+    chosenLiElem.querySelectorAll('.collapse-expand').forEach(element => {
+        element.classList.add('expand-book-content');
+    }); 
 
+    chosenLiElem.querySelector('.close').addEventListener('click', (evt) => {
+            console.log(evt.target);
+            chosenLiElem.querySelectorAll('.show_hide').forEach(element => {
+                element.classList.add('hidden');
+            });
+            chosenLiElem.querySelectorAll('.collapse-expand').forEach(element => {
+                element.classList.remove('expand-book-content');
+            });
+    })
 }
 
 
